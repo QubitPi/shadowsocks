223b2ec027c8ce22ffab61ee6f4a403e0a044975
Jiaqi Liu <jack20220723@gmail.com>
2025-07-09 02:02:57 +0800

    Add hero image (#7)

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,6 +1,7 @@
-// Use VitePress's default theme without loading the default fonts
+/**
+ * Copyright 2025 Jiaqi Liu. All rights reserved.
+ */
 import DefaultTheme from 'vitepress/theme-without-fonts';
-// Load your custom CSS file where you'll set your own fonts
 import './custom.css';
 
 export default DefaultTheme;
